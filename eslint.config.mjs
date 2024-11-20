aca0931b0bf676ec272ef5676aae4688f28876f3
Khristian <zapdos.knight@gmail.com>
2024-11-20 18:38:06 +0700

    terbaru

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import daStyle from 'eslint-config-dicodingacademy';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  daStyle,
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.node} }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  daStyle,
 ];
+
+import daStyle from 'eslint-config-dicodingacademy';
+
+// export default [
+//   daStyle,
+//   // other config style
+// ];
