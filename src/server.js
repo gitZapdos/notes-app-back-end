b5339754efdf771180635d0ee1ffccd09173bc6e
Khristian <zapdos.knight@gmail.com>
2025-02-14 20:33:01 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,14 +2,22 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// notes
 const notes = require('./api/notes');
 // const NotesService = require('./services/inMemory/NotesService');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -20,18 +28,27 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
-  
+
     // penanganan client error secara internal.
     if (response instanceof ClientError) {
       const newResponse = h.response({
@@ -41,7 +58,7 @@ const init = async () => {
       newResponse.code(response.statusCode);
       return newResponse;
     }
-      
+
     return h.continue;
   });
 
